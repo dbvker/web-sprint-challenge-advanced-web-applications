eb2d32f3c0f160a85297f0ab63b4ef7de59b2e92
Dylan Baker <dylanmbaker@yahoo.com>
2022-01-28 14:38:11 -0600

    finished logout component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Header = () => {    
+const Header = () => {
     return(
         <HeaderStyle>
             <p>Blogger Pro</p>
--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
-const Logout = () => {        
-    return(<div>Logout</div>);
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+const Logout = () => {     
+    let { push } = useHistory();
+
+    useEffect(() => {
+        axiosWithAuth()
+            .post('/logout')
+            .then(() => {
+                localStorage.removeItem('token');
+                localStorage.removeItem('role');
+                localStorage.removeItem('username');
+                push('/');
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
+
+    return <div></div>;
 }
 
 export default Logout;
